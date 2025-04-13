e36d06a2402fe4f63939f1f01a85626119679b67
Κωσταντίνος Αχιλλέας Μούσιας <kwstantinosm@hotmail.com>
2025-04-13 21:21:08 +0300

    Web servlet Created

---
+++ src/ProxySystem.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/proxy-system")
+public class ProxySystem extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public ProxySystem() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.getWriter().println("ProxySystem GET response");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.getWriter().println("ProxySystem POST response");
+    }
+}
