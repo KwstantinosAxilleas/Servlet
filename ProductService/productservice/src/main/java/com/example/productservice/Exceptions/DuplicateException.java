647fb9132bc03468a6ed0607260b5862f3842367
Κωσταντίνος Αχιλλέας Μούσιας <kwstantinosm@hotmail.com>
2025-04-25 14:49:12 +0300

    a

--- ProductService/productservice/src/main/java/com/example/productservice/Exceptions/DuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.example.productservice.Exceptions;
-
-public class DuplicateException extends RuntimeException{
-
-    public DuplicateException(String name, Class<?> entity){
-        super("The product with name '" + name + "' already exists in our records, use different product name.");
-    }
-}
